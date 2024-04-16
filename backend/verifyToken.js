9a20d75f378680e73c0943afe4193c2df10f57e4
abhishekkalgudi <abhishekkalgudi03@gmail.com>
2024-04-16 23:16:51 +0530

    jwt token verification middleware implemented

---
+++ backend/verifyToken.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.token;
+    if(!token) {
+        return res.status(401).json({ message: "Access Denied. No token provided. "});
+    }
+    jwt.verify(token, process.env.SECRET, async (err, data) => {
+        if(err) {
+            return res.status(403).json({ message: "Invalid token. "});
+        }
+        console.log("Passed");
+        req.userId = data.id;
+        next();
+    })
+}
+
+module.exports = verifyToken;
