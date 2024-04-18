824b8799061e728ad8068f3ba85a8cfe3bfb1a5e
abhishekkalgudi <abhishekkalgudi03@gmail.com>
2024-04-19 02:15:29 +0530

    user login functionality with backend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
+import { useContext, useState } from "react";
+import axios from "axios";
+import { URL } from './../url';
+import { UserContext } from "../context/UserContext";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { setUser } = useContext(UserContext);
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(URL + "/api/auth/login", { email, password }, { withCredentials: true });
+      console.log(res);
+      setUser(res.data);
+      navigate("/");
+    } catch (error) {
+      setError(true);
+    }
+  }
+
   return (
     <>
       <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
@@ -19,15 +40,18 @@ const Login = () => {
             className="w-full px-4 py-2 border-2 border-black outline-0 rounded-lg"
             type="text"
             placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             className="w-full px-4 py-2 border-2 border-black outline-0 rounded-lg"
             type="password"
             placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <button className="w-full text-lg px-4 py-4 font-bold text-white bg-black rounded-lg hover:bg-blue-600">
+          <button onClick={handleLogin} className="w-full text-lg px-4 py-4 font-bold text-white bg-black rounded-lg hover:bg-blue-600">
             Login
           </button>
+          {error && <h3 className="text-red-500">Invalid Credentials</h3>}
           <div className="flex justify-center items-center space-x-2">
             <h1>New Here?</h1>
             <Link to="/register" className=" text-blue-600">
