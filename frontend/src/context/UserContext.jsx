5e6b786d3eaddc5a7bbbf527862855e8fb92959d
abhishekkalgudi <abhishekkalgudi03@gmail.com>
2024-04-19 02:12:19 +0530

    usercontext, user login status on refresh handled

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+import { URL } from "./../url";
+
+export const UserContext = createContext({});
+
+export const UserContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const handleRefresh = async () => {
+      try {
+        const res = await axios.get(URL + "/api/auth/refetch", { withCredentials: true });
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    handleRefresh();
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
