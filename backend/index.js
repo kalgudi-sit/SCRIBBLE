01d411b3095102256ee020be1d5a0d7d237975d5
abhishekkalgudi <abhishekkalgudi03@gmail.com>
2024-04-21 14:46:25 +0530

    multer and path module backend config for image upload

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const multer = require("multer");
 const cors = require("cors");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/users");
@@ -13,29 +15,44 @@ const app = express();
 // Middlewares
 dotenv.config();
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images"))); // configuring Express to serve static files located in a directory when a specific URL path is requested
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
-app.use((req,res,next) => {
-    console.log(req.path + " " + req.method);
-    next();
+app.use((req, res, next) => {
+  console.log(req.path + " " + req.method);
+  next();
 });
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/comments", commentRouter);
 
-
 // Database Connection and Config
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("Connected to Database Successfully");
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to Database Successfully");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.img);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json({ message: "Image uploaded successfully " });
+});
 
 app.listen(process.env.PORT, () => {
-    connectDB();
-    console.log(`Server running on port ${process.env.PORT}`);
-})
+  connectDB();
+  console.log(`Server running on port ${process.env.PORT}`);
+});
